85d92d43f62ca43709ead689f203cea236acccf3
wyt151 <janet.ytwu@gmail.com>
2025-03-11 11:23:26 +0800

    Step 8: update choices

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q,F
+from listings.choices import price_choices, bedroom_choices, district_choices
 
 # Create your views here.
 def listings(request):
   #listings = Listing.objects.all()
   listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+  #listings = Listing.objects.filter(Q(district='Pok Fu Lam') | Q(district='Tai Koo'))
+  #listings = Listing.objects.filter(Q(district='Pok Fu Lam') & ~Q(bedrooms='2'))
+  #listings = Listing.objects.filter(district=F('address'))
   paginator = Paginator(listings, 3)
   page = request.GET.get('page')
   paged_listings = paginator.get_page(page)
--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, district_choices
 
 # Create your views here.
 def index(request):
   #return HttpResponse("<h1>Hello World!</h1>") #testing
   listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-  context = {'listings':listings}
+  context = { 'listings':listings,
+              'price_choices': price_choices,
+              'bedroom_choices': bedroom_choices,
+              'district_choices': district_choices}
   return render(request, 'pages/index.html',context)
 
 def about(request):
@@ -15,4 +19,5 @@ def about(request):
   realtors = Realtor.objects.order_by('-hire_date')
   mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
   context = {"realtors" : realtors, "mvp_realtors" : mvp_realtors}
+  #print(mvp_realtors)
   return render(request, 'pages/about.html',context)
