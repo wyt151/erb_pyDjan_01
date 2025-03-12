15408df0ebcdaa99df96ffdbb8652cb950abe27e
wyt151 <janet.ytwu@gmail.com>
2025-03-12 14:18:02 +0800

    Step 9:update search

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms import NumberInput
+from django.db import models
 
 # Register your models here.
 from .models import Listing
@@ -6,8 +8,12 @@ class ListingAdmin(admin.ModelAdmin):
   list_display = ('id','title','is_published','price','list_date','realtor')
   list_display_links = 'id','title'
   list_filter = 'realtor',
-  list_editable = 'is_published',
+  list_editable = 'is_published', 'price'
   search_fields = 'title','description','address','price'
   list_per_page = 25
   ordering = 'id',
+  #ordering = ['-id'],
+  formfield_overrides = {
+    models.IntegerField: {'widget' : NumberInput(attrs={'size':'10'})},
+  }
 admin.site.register(Listing,ListingAdmin)
--- listings/views.py
+++ listings/views.py
@@ -25,4 +25,32 @@ def listing(request,listing_id):
   return render(request, 'listings/listing.html', context)
 
 def search(request):
-  return render(request,'listings/search.html')
+  queryset_list = Listing.objects.order_by('-list_date')
+  if 'keywords' in request.GET:
+    keywords = request.GET['keywords']
+    if keywords:
+      queryset_list = queryset_list.filter(description__icontains=keywords)
+  if 'title' in request.GET:
+    title = request.GET['title']
+    if title:
+      queryset_list = queryset_list.filter(title__icontains=title)
+  if 'district' in request.GET:
+    district = request.GET['district']
+    if district:
+      queryset_list = queryset_list.filter(district__iexact=district)
+  if 'bedrooms' in request.GET:
+    bedrooms = request.GET['bedrooms']
+    if bedrooms:
+      queryset_list = queryset_list.filter(bedrooms__gte=bedrooms)
+  if 'price' in request.GET:
+    price = request.GET['price']
+    if price:
+      queryset_list = queryset_list.filter(price__lte=price)
+  context = {
+    'price_choices' : price_choices,
+    'district_choices' : district_choices,
+    'bedroom_choices' : bedroom_choices,
+    'listings' : queryset_list,
+    'values' : request.GET
+  }
+  return render(request,'listings/search.html',context)
